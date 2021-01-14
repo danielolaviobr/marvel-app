8c7123ba43f961ac20c4bfca6d0e70f18e8a0306
Daniel Olavio Ferreira <danielolaviobr@gmail.com>
2021-01-14 13:10:52 -0300

    FIX: Card styling

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -23,9 +23,9 @@ const Card: React.FC<CardProps> = ({ image, title, description, id, type }) => {
     <div
       className="flex flex-col items-center p-4 m-8 bg-gray-100 rounded-md shadow-md cursor-pointer max-w-50 md:max-w-xl md:items-start hover:opacity-90 md:min-w-40 md:flex-row min-w-50"
       onClick={() => navigateTo(id)}>
-      <img className="mx-4 mt-4 rounded" src={image} alt={title} />
+      <img className="rounded shadow-md" src={image} alt={title} />
       <div className="flex flex-col items-center mx-4">
-        <h1 className="mt-4 mb-4 text-2xl font-bold text-center md:mt-0 md:self-start">
+        <h1 className="mb-4 text-2xl font-bold text-center md:mt-0 md:self-start">
           {title || "Personagem não encontrado"}
         </h1>
         <p className="invisible hidden text-justify md:visible md:flex">
